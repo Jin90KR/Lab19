b5d60d1cf84546fb44173e0726f7b6fcd6104f31
hwanjin lee <hwanjinlee90@gmail.com>
2023-10-01 16:42:19 -0700

    update: .then

--- index.js
+++ index.js
@@ -5,16 +5,39 @@ const dogCards = document.querySelector(".cards");
 const moreLoadBtn = document.getElementById("moreload_btn");
 let pageNum = 0;
 
-async function getRandomDogs() {
+// async function getRandomDogs() {
+//     pageNum = pageNum + 1;
+//     let inputValue = inputData.value;
+//     const data = await fetch(`https://api.unsplash.com/search/photos?page=${pageNum}&query=${inputValue}&client_id=${apiKey}&per_page=12`)
+//     const jsonData = await data.json();
+//     console.log(pageNum);
+//     console.log(inputValue);
+//     console.log(jsonData.results);
+   
+//     jsonData.results.forEach(boximg => {
+//         console.log(boximg)
+//         const cardElement = document.createElement("div");
+//         const imgElement = document.createElement("img");
+//         const descriptionElement = document.createElement("p");
+//         cardElement.classList.add("card");
+//         imgElement.src = boximg.links.download;
+//         descriptionElement.innerText = boximg.alt_description;
+       
+//         dogCards.appendChild(cardElement);
+//         cardElement.appendChild(imgElement);
+//         cardElement.appendChild(descriptionElement);
+//         console.log(imgElement)
+//     })
+// }
+
+function getRandomDogs() {
     pageNum = pageNum + 1;
     let inputValue = inputData.value;
-    const data = await fetch(`https://api.unsplash.com/search/photos?page=${pageNum}&query=${inputValue}&client_id=${apiKey}&per_page=12`)
-    const jsonData = await data.json();
-    console.log(pageNum);
-    console.log(inputValue);
-    console.log(jsonData.results);
-   
-    jsonData.results.forEach(boximg => {
+
+    fetch(`https://api.unsplash.com/search/photos?page=${pageNum}&query=${inputValue}&client_id=${apiKey}&per_page=12`)
+    .then((data) => data.json())
+    .then((result) => {
+        result.results.forEach(boximg => {
         console.log(boximg)
         const cardElement = document.createElement("div");
         const imgElement = document.createElement("img");
@@ -27,6 +50,7 @@ async function getRandomDogs() {
         cardElement.appendChild(imgElement);
         cardElement.appendChild(descriptionElement);
         console.log(imgElement)
+        })
     })
 }
 
